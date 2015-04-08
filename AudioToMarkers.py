35793457d51d0f9aa224889c03037f876ebc03d5
Jacques Lucke <mail@jlucke.com>
2015-04-08 21:50:22 +0200

    make addon

--- AudioToMarkers.py
+++ AudioToMarkers.py
@@ -1,3 +1,35 @@
+'''
+Copyright (C) 2014 Jacques Lucke
+mail@jlucke.com
+
+Created by Jacques Lucke
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+bl_info = {
+    "name": "Audio to Markers",
+    "description": "Work effectively with audio data in the graph editor.",
+    "author": "Your Name",
+    "version": (0, 0, 1),
+    "blender": (2, 74, 0),
+    "location": "View3D",
+    "warning": "This is an unstable version",
+    "wiki_url": "",
+    "category": "Animation" }
+
+
 import bpy
 import os.path
 import math
